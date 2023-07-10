66963d40130b34ce7426922c8bb93a24a8965ec9
Junichi Kato <j5ik2o@gmail.com>
2023-07-11 00:08:02 +0900

    RMUにDAOを追加

---
+++ migrations/20230710130002_init.sql
@@ -0,0 +1,32 @@
+-- Add migration script here
+
+CREATE TABLE `threads`
+(
+    `id`         varchar(64) NOT NULL,
+    `owner_id`   varchar(64) NOT NULL,
+    `created_at` datetime    NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE `members`
+(
+    `thread_id`  varchar(64) NOT NULL,
+    `account_id` varchar(64) NOT NULL,
+    `created_at` datetime    NOT NULL,
+    PRIMARY KEY (`thread_id`, `account_id`),
+    FOREIGN KEY (`thread_id`) REFERENCES threads (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE `messages`
+(
+    `id`         varchar(64) NOT NULL,
+    `thread_id`  varchar(64) NOT NULL,
+    `account_id` varchar(64) NOT NULL,
+    `text`       TEXT        NOT NULL,
+    `created_at` datetime    NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`thread_id`) REFERENCES threads (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
