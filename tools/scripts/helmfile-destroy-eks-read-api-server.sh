76b23498d2241042e1efaad240f557c09c7f9250
Junichi Kato <j5ik2o@gmail.com>
2023-07-15 11:31:36 +0900

    read-api-serverを追加

---
+++ tools/scripts/helmfile-destroy-eks-read-api-server.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# shellcheck disable=SC2046
+cd $(dirname "$0") || exit
+
+if [[ ! -e ../../env.sh ]]; then
+    echo "env.sh is not found."
+    exit 1
+fi
+
+# shellcheck disable=SC2034
+OUTPUT_ENV=1
+
+source ../../env.sh
+
+export AWS_DEFAULT_REGION=$AWS_REGION
+
+pushd ../helmfile.d
+
+helmfile --namespace ceer --selector group=read-api-server -e "${PREFIX}-${APPLICATION_NAME}-eks" destroy
+
+popd
