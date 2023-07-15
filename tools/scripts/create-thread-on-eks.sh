a07f982bfc0cd89ed74ba458f2b559be5cbaf515
Junichi Kato <j5ik2o@gmail.com>
2023-07-15 21:56:36 +0900

    rename

--- tools/scripts/create-thread-on-eks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -ue
-
-PORT=${PORT:-443}
-HOST_URL=${HOST_URL:-https://write-ceer-j5ik2o.cwtest.info}
-
-curl -X POST -H "Content-Type: application/json" -d "{\"name\":\"test\",\"executor_id\":{\"value\":\"01H4J5WDZDXYJ4NWRDT5AR1J6E\"}}" ${HOST_URL}:${PORT}/threads/create
--- tools/scripts/create-thread-on-local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -ue
-
-PORT=${PORT:-8080}
-HOST_URL=${HOST_URL:-http://localhost}
-
-curl -X POST -H "Content-Type: application/json" -d "{\"name\":\"test\",\"executor_id\":{\"value\":\"01H4J5WDZDXYJ4NWRDT5AR1J6E\"}}" ${HOST_URL}:${PORT}/threads/create
