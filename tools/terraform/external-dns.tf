d75079c40621954d074e3d6d376511ad8244d243
Junichi Kato <j5ik2o@gmail.com>
2023-07-15 21:52:34 +0900

    external-dnsをデプロイしないように修正

--- tools/terraform/external-dns.tf
+++ tools/terraform/external-dns.tf
@@ -1,13 +1,12 @@
 module "external-dns" {
   source                      = "./eks-external-dns"
-  create = true
+  create                      = false
   eks_cluster_oidc_issuer_url = module.eks.cluster_oidc_issuer_url
   zone_id                     = var.zone_id
   zone_name                   = var.zone_name
-
-  policy = "upsert-only"
-  interval = "30m"
-  trigger_loop_on_event = true
+  policy                      = "sync"
+  interval                    = "30m"
+  trigger_loop_on_event       = true
 
   depends_on = [
     module.eks
