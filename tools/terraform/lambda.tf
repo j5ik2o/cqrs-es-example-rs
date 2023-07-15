aa79416018dc48a03661141cc00cfe87532e8263
Junichi Kato <j5ik2o@gmail.com>
2023-07-15 09:52:51 +0900

    tf内容見直し

--- tools/terraform/lambda.tf
+++ tools/terraform/lambda.tf
@@ -3,23 +3,30 @@ module "lambda_function" {
 
   create = true
 
-  function_name = "read-model-updater"
+  create_function = true
+
+  function_name = "${var.prefix}-lambda-read-model-updater"
   description   = "read-model-updater"
 
   create_package = false
 
-  image_uri     = "738575627980.dkr.ecr.ap-northeast-1.amazonaws.com/aht9aa1e-ecr-ceer-read-model-updater:latest-amd64"
-  package_type  = "Image"
+  timeout = 60
+
+#  image_uri     = "738575627980.dkr.ecr.ap-northeast-1.amazonaws.com/aht9aa1e-ecr-ceer-read-model-updater:latest-amd64"
+#  package_type  = "Image"
   architectures = ["x86_64"]
 
-  environment_variables = {
-    "RUST_LOG"           = "debug"
-    "RUST_BACKTRACE"     = "full"
-    "APP__DATABASE__URL" = "mysql://${module.aurora.cluster_master_username}:${module.aurora.cluster_master_password}@${module.aurora.cluster_endpoint}/${module.aurora.cluster_database_name}"
-  }
+  handler                 = "bootstrap"
+  runtime                 = "provided.al2"
+  local_existing_package  = "${path.module}/../../target/lambda/cqrs-es-example-read-model-updater/bootstrap-0d385892717f221ea18475226115fedc.zip"
+  ignore_source_code_hash = true
 
-  attach_policy = true
-  policy        = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+  environment_variables = {
+    "RUST_LOG"              = "debug"
+    "RUST_BACKTRACE"        = "full"
+    "APP__DATABASE__URL"    = "mysql://${module.aurora.cluster_master_username}:${module.aurora.cluster_master_password}@${module.aurora.cluster_endpoint}/${module.aurora.cluster_database_name}"
+    "APP__AWS__REGION_NAME" = "ap-northeast-1"
+  }
 
   event_source_mapping = {
     dynamodb = {
@@ -27,6 +34,26 @@ module "lambda_function" {
       starting_position = "LATEST"
     }
   }
+
+  allowed_triggers = {
+    dynamodb = {
+      principal  = "dynamodb.amazonaws.com"
+      source_arn = module.event_sourcing.aws_dynamodb_table_journal_stream_arn
+    }
+  }
+  create_current_version_allowed_triggers = false
+  attach_network_policy = true
+
+
+  vpc_subnet_ids         = module.vpc.private_subnets
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+
+  attach_policies    = true
+  number_of_policies = 1
+
+  policies = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaDynamoDBExecutionRole",
+  ]
 
   depends_on = [
     module.aurora,
