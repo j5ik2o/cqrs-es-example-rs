701be76c8e2e1e4ebe90a0483208d7db54cdf9ae
Junichi Kato <j5ik2o@gmail.com>
2023-07-15 17:23:36 +0900

    スクリプトを調整

--- tools/scripts/docker-build-read-api-server.sh
+++ tools/scripts/docker-build-read-api-server.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
-# FYI: https://zenn.dev/kinzal/articles/9ee60ebbebc29c
-
 set -eu
 
-# shellcheck disable=SC2046
 cd $(dirname "$0") || exit
 
-F_OPTION=""
-while getopts f OPT; do
-  # shellcheck disable=SC2220
-  case ${OPT} in
-  "f") F_OPTION=1 ;;
-  esac
-done
+if [ "$#" -gt 0 ]; then
+    ARCH=$1
+else
+    ARCH=$(uname -m)
+fi
 
-LOCAL_REPO_NAME=j5ik2o/cqrs-es-example-rs-read-api-server
+echo "ARCH=${ARCH}"
+
+APP_NAME=read-api-server
+LOCAL_REPO_NAME=j5ik2o/cqrs-es-example-rs-${APP_NAME}
+
 TAG=latest
 LOCAL_URI=${LOCAL_REPO_NAME}:${TAG}
 LOCAL_AMD64_URI=${LOCAL_REPO_NAME}:${TAG}-amd64
@@ -23,14 +22,33 @@ LOCAL_ARM64_URI=${LOCAL_REPO_NAME}:${TAG}-arm64
 
 pushd ../../
 
+IS_ALL=0
+if [ "$ARCH" == "all" ]; then
+    IS_ALL=1
+fi
+
+PIDS=()
+
+if [ "$ARCH" == "arm64" ] || [ "$IS_ALL" -eq 1 ]; then
+
 docker buildx build --builder amd-arm --platform linux/arm64 \
   --build-context messense/rust-musl-cross:arm64-musl=docker-image://messense/rust-musl-cross:aarch64-musl \
-  -t $LOCAL_ARM64_URI --load -f query/read-api-server/Dockerfile .
+  -t $LOCAL_ARM64_URI --load -f query/${APP_NAME}/Dockerfile . &
+PIDS+=($!)
 
-if [[ "$F_OPTION" == 1 ]]; then
+fi
+
+if [ "$ARCH" == "amd64" ] || [ "$IS_ALL" -eq 1 ]; then
+
 docker buildx build --builder amd-arm --platform linux/amd64 \
   --build-context messense/rust-musl-cross:amd64-musl=docker-image://messense/rust-musl-cross:x86_64-musl \
-  -t $LOCAL_AMD64_URI --load -f query/read-api-server/Dockerfile .
+  -t $LOCAL_AMD64_URI --load -f query/${APP_NAME}/Dockerfile . &
+PIDS+=($!)
+
 fi
+
+for PID in "${PIDS[@]}"; do
+    wait "$PID"
+done
 
 popd
