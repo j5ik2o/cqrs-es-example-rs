531742f8b70b2bf6de9a0a3773a63127e512114b
Junichi Kato <j5ik2o@gmail.com>
2023-07-15 21:54:13 +0900

    rename

---
+++ tools/scripts/curl-post-create-thread-on-eks.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -ue
+
+HOST_URL=https://write-ceer-j5ik2o.cwtest.info
+
+curl -v -X POST \
+	-H "Content-Type: application/json" \
+	-d "{\"name\":\"test\",\"executor_id\":{\"value\":\"01H4J5WDZDXYJ4NWRDT5AR1J6E\"}}" \
+	${HOST_URL}/threads/create
---
+++ tools/scripts/curl-post-create-thread-on-local.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -ue
+
+curl -v -X POST \
+	-H "Content-Type: application/json" \
+	-d "{\"name\":\"test\",\"executor_id\":{\"value\":\"01H4J5WDZDXYJ4NWRDT5AR1J6E\"}}" \
+	http://localhost:8080/threads/create
