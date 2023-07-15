4a969d3ea41f5c10eeb6e5d221a9bcbe0257a4cb
Junichi Kato <j5ik2o@gmail.com>
2023-07-15 09:49:59 +0900

    ハッシュ値をファイル名に入れるように変更

--- tools/scripts/cargo-lambda-build-read-model-updater.sh
+++ tools/scripts/cargo-lambda-build-read-model-updater.sh
@@ -6,6 +6,11 @@ cd $(dirname "$0") || exit
 
 pushd ../../
 
+rm -fr target/lambda/release/cqrs-es-example-read-model-updater
 cargo lambda build -p cqrs-es-example-read-model-updater --release --output-format zip
+MD5=$(md5 -q target/lambda/cqrs-es-example-read-model-updater/bootstrap.zip)
+cp target/lambda/cqrs-es-example-read-model-updater/bootstrap.zip target/lambda/cqrs-es-example-read-model-updater/bootstrap-${MD5}.zip
+echo "target/lambda/cqrs-es-example-read-model-updater/bootstrap-${MD5}.zip"
+echo "MD5=${MD5}"
 
 popd
