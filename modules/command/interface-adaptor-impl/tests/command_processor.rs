d868fa4a9721a9c05a76967ba8ac6a30d92d78d6
Junichi Kato <j5ik2o@gmail.com>
2024-02-23 21:11:07 +0900

    fix: build error

--- modules/command/interface-adaptor-impl/tests/command_processor.rs
+++ modules/command/interface-adaptor-impl/tests/command_processor.rs
@@ -5,7 +5,6 @@ use command_domain::group_chat::Members;
 use command_domain::group_chat::{GroupChatName, MemberRole, Message};
 use command_domain::user_account::UserAccountId;
 use command_interface_adaptor_if::*;
-use command_interface_adaptor_impl::controllers::GroupChatIdPresenter;
 use command_processor::command_processor::GroupChatCommandProcessor;
 use common::*;
 
@@ -21,12 +20,9 @@ async fn test_group_chat_create() {
   let name = GroupChatName::new("ABC").unwrap();
   let admin_id = UserAccountId::new();
   let _members = Members::new(admin_id.clone());
-  let mut command_processor = GroupChatCommandProcessor::new(&mut repository);
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
+  let mut command_processor = GroupChatCommandProcessor::new(repository);
   // When
-  let result = command_processor
-    .create_group_chat(&mut group_chat_id_presenter, name, admin_id)
-    .await;
+  let result = command_processor.create_group_chat(name, admin_id).await;
 
   // Then
   assert!(result.is_ok());
@@ -42,25 +38,22 @@ async fn test_group_chat_rename() {
   let name = GroupChatName::new("ABC").unwrap();
   let admin_id = UserAccountId::new();
   let _members = Members::new(admin_id.clone());
-  let mut command_processor = GroupChatCommandProcessor::new(&mut repository);
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
+  let mut command_processor = GroupChatCommandProcessor::new(repository);
 
-  command_processor
-    .create_group_chat(&mut group_chat_id_presenter, name.clone(), admin_id.clone())
+  let id = command_processor
+    .create_group_chat(name.clone(), admin_id.clone())
     .await
     .unwrap();
-  let id = group_chat_id_presenter.group_chat_id();
 
   // When
   let name = GroupChatName::new("DEF").unwrap();
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
   let result = command_processor
-    .rename_group_chat(&mut group_chat_id_presenter, id.clone(), name.clone(), admin_id)
+    .rename_group_chat(id.clone(), name.clone(), admin_id)
     .await;
 
   // Then
   assert!(result.is_ok());
-  let group_chat = repository.find_by_id(id).await.unwrap().unwrap();
+  let group_chat = repository.find_by_id(&id).await.unwrap().unwrap();
   assert_eq!(*group_chat.name(), name);
 }
 
@@ -75,31 +68,22 @@ async fn test_group_chat_add_member() {
   let name = GroupChatName::new("ABC").unwrap();
   let admin_id = UserAccountId::new();
   let _members = Members::new(admin_id.clone());
-  let mut command_processor = GroupChatCommandProcessor::new(&mut repository);
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
-  command_processor
-    .create_group_chat(&mut group_chat_id_presenter, name.clone(), admin_id.clone())
+  let mut command_processor = GroupChatCommandProcessor::new(repository);
+  let id = command_processor
+    .create_group_chat(name.clone(), admin_id.clone())
     .await
     .unwrap();
-  let id = group_chat_id_presenter.group_chat_id();
   let user_account_id = UserAccountId::new();
   let role = MemberRole::Member;
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
 
   // When
   let result = command_processor
-    .add_member(
-      &mut group_chat_id_presenter,
-      id.clone(),
-      user_account_id.clone(),
-      role,
-      admin_id.clone(),
-    )
+    .add_member(id.clone(), user_account_id.clone(), role, admin_id.clone())
     .await;
 
   // Then
   assert!(result.is_ok());
-  let group_chat = repository.find_by_id(id).await.unwrap().unwrap();
+  let group_chat = repository.find_by_id(&id).await.unwrap().unwrap();
   assert!(group_chat.members().is_administrator(&admin_id));
   assert!(group_chat.members().is_member(&user_account_id));
 }
@@ -116,17 +100,13 @@ async fn test_group_chat_remove_member() {
   // Given
   let name = GroupChatName::new("ABC").unwrap();
   let _members = Members::new(admin_id.clone());
-  let mut command_processor = GroupChatCommandProcessor::new(&mut repository);
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
-  command_processor
-    .create_group_chat(&mut group_chat_id_presenter, name.clone(), admin_id.clone())
+  let mut command_processor = GroupChatCommandProcessor::new(repository);
+  let id = command_processor
+    .create_group_chat(name.clone(), admin_id.clone())
     .await
     .unwrap();
-  let id = group_chat_id_presenter.group_chat_id();
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
   command_processor
     .add_member(
-      &mut group_chat_id_presenter,
       id.clone(),
       user_account_id.clone(),
       MemberRole::Member,
@@ -134,21 +114,15 @@ async fn test_group_chat_remove_member() {
     )
     .await
     .unwrap();
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
 
   // When
   let result = command_processor
-    .remove_member(
-      &mut group_chat_id_presenter,
-      id.clone(),
-      user_account_id.clone(),
-      admin_id.clone(),
-    )
+    .remove_member(id.clone(), user_account_id.clone(), admin_id.clone())
     .await;
 
   // Then
   assert!(result.is_ok());
-  let group_chat = repository.find_by_id(id).await.unwrap().unwrap();
+  let group_chat = repository.find_by_id(&id).await.unwrap().unwrap();
   assert!(group_chat.members().is_administrator(&admin_id));
   assert!(!group_chat.members().is_member(&user_account_id));
 }
@@ -163,43 +137,28 @@ async fn test_group_chat_post_message() {
   let name = GroupChatName::new("ABC").unwrap();
   let admin_id = UserAccountId::new();
   let _members = Members::new(admin_id.clone());
-  let mut command_processor = GroupChatCommandProcessor::new(&mut repository);
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
-  command_processor
-    .create_group_chat(&mut group_chat_id_presenter, name.clone(), admin_id.clone())
+  let mut command_processor = GroupChatCommandProcessor::new(repository);
+  let id = command_processor
+    .create_group_chat(name.clone(), admin_id.clone())
     .await
     .unwrap();
-  let id = group_chat_id_presenter.group_chat_id();
   let user_account_id = UserAccountId::new();
   let role = MemberRole::Member;
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
   command_processor
-    .add_member(
-      &mut group_chat_id_presenter,
-      id.clone(),
-      user_account_id.clone(),
-      role,
-      admin_id.clone(),
-    )
+    .add_member(id.clone(), user_account_id.clone(), role, admin_id.clone())
     .await
     .unwrap();
   let text = "ABC".to_string();
   let message = Message::new(text.clone(), user_account_id.clone());
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
 
   // When
   let result = command_processor
-    .post_message(
-      &mut group_chat_id_presenter,
-      id.clone(),
-      message,
-      user_account_id.clone(),
-    )
+    .post_message(id.clone(), message, user_account_id.clone())
     .await;
 
   // Then
   assert!(result.is_ok());
-  let group_chat = repository.find_by_id(id).await.unwrap().unwrap();
+  let group_chat = repository.find_by_id(&id).await.unwrap().unwrap();
   assert_eq!(group_chat.messages().len(), 1);
   assert_eq!(
     group_chat.messages().get_at(0).unwrap().breach_encapsulation_of_text(),
@@ -217,44 +176,27 @@ async fn test_group_chat_delete_message() {
   let name = GroupChatName::new("ABC").unwrap();
   let admin_id = UserAccountId::new();
   let _members = Members::new(admin_id.clone());
-  let mut command_processor = GroupChatCommandProcessor::new(&mut repository);
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
-  command_processor
-    .create_group_chat(&mut group_chat_id_presenter, name.clone(), admin_id.clone())
+  let mut command_processor = GroupChatCommandProcessor::new(repository);
+  let id = command_processor
+    .create_group_chat(name.clone(), admin_id.clone())
     .await
     .unwrap();
-  let id = group_chat_id_presenter.group_chat_id();
   let user_account_id = UserAccountId::new();
   let role = MemberRole::Member;
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
   command_processor
-    .add_member(
-      &mut group_chat_id_presenter,
-      id.clone(),
-      user_account_id.clone(),
-      role,
-      admin_id.clone(),
-    )
+    .add_member(id.clone(), user_account_id.clone(), role, admin_id.clone())
     .await
     .unwrap();
   let text = "ABC".to_string();
   let message = Message::new(text.clone(), user_account_id.clone());
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
   command_processor
-    .post_message(
-      &mut group_chat_id_presenter,
-      id.clone(),
-      message.clone(),
-      user_account_id.clone(),
-    )
+    .post_message(id.clone(), message.clone(), user_account_id.clone())
     .await
     .unwrap();
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
 
   // When
   let result = command_processor
     .delete_message(
-      &mut group_chat_id_presenter,
       id.clone(),
       message.breach_encapsulation_of_id().clone(),
       user_account_id.clone(),
@@ -263,7 +205,7 @@ async fn test_group_chat_delete_message() {
 
   // Then
   assert!(result.is_ok());
-  let group_chat = repository.find_by_id(id).await.unwrap().unwrap();
+  let group_chat = repository.find_by_id(&id).await.unwrap().unwrap();
   assert_eq!(group_chat.messages().len(), 0);
 }
 
@@ -278,18 +220,13 @@ async fn test_group_chat_destroy() {
   let name = GroupChatName::new("ABC").unwrap();
   let admin_id = UserAccountId::new();
   let _members = Members::new(admin_id.clone());
-  let mut command_processor = GroupChatCommandProcessor::new(&mut repository);
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
-  command_processor
-    .create_group_chat(&mut group_chat_id_presenter, name, admin_id.clone())
+  let mut command_processor = GroupChatCommandProcessor::new(repository);
+  let id = command_processor
+    .create_group_chat(name, admin_id.clone())
     .await
     .unwrap();
-  let id = group_chat_id_presenter.group_chat_id().clone();
-  let mut group_chat_id_presenter = GroupChatIdPresenter::new();
   // When
-  let result = command_processor
-    .delete_group_chat(&mut group_chat_id_presenter, id, admin_id)
-    .await;
+  let result = command_processor.delete_group_chat(id, admin_id).await;
 
   // Then
   assert!(result.is_ok());
