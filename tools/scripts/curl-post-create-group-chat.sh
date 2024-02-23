e21546422592f71c4abca200dadc36a5762d6dc3
Junichi Kato <j5ik2o@gmail.com>
2024-02-24 00:27:28 +0900

    fix: scripts & Makefile.toml

--- tools/scripts/curl-post-create-group-chat.sh
+++ tools/scripts/curl-post-create-group-chat.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-curl -s -X 'POST' \
-    "${WRITE_API_SERVER_BASE_URL}/group-chats/create" \
-    -H 'accept: application/json' \
-    -H 'Content-Type: application/json' \
-    -d '{ "executor_id": "01H42K4ABWQ5V2XQEP3A48VE0Z", "name": "group-chat-example" }'
+curl -s -X POST -H "Content-Type: application/json" \
+	${WRITE_API_SERVER_BASE_URL}/query \
+	-d @- <<EOS
+{
+  "query": "mutation CreateGroupChat(\$input: CreateGroupChatInput!) { createGroupChat(input: \$input) { groupChatId } }",
+  "variables": {
+    "input": {
+      "name": "group-chat-example",
+      "executorId": "01H42K4ABWQ5V2XQEP3A48VE0Z"
+    }
+  }
+}
+EOS
